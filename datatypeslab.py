97f4e5ec9a52a3bd10411ae344008cb8217dc1d9
kibytes <mutuab@gmail.com>
2017-04-05 12:21:33 +0300

    Add files via upload

--- datatypeslab.py
+++ datatypeslab.py
@@ -5,17 +5,16 @@ def data_type(d):
         return len(d)
     elif isinstance(d, int):
         if d < 100:
-            return "less than 100"
+            return 'less than 100'
         elif d > 100:
-            return "more than 100"
+            return 'more than 100'
         else:
-            return "integer equal to 100"
+            return 'equal to 100'
     elif isinstance(d, list):
         try:
-            return d[3]
+            return d[2]
         except IndexError:
             return None
     else:
-        return "no value"
+        return 'no value'
 
-#print data_type("ght")
